7518ff63dc3d94a0889ff516295a39cf996d13d6
MohammedOsamma <120668625+MohammedOsamma@users.noreply.github.com>
2024-07-27 23:09:54 +0300

    Simple Math Expression Evaluator

---
+++ Simple Math Expression Evaluator/Simple Math Expression Evaluator/ExperssionParser.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simple_Math_Expression_Evaluator
+{
+    public static  class ExperssionParser
+    {
+        private const string MathSymbol = "+*/%^";
+
+        public static MathExpression Parse (string input)
+        {
+            var expr = new MathExpression ();
+            string token = "";
+            bool leftSideInitialized =false;
+
+            for (var i= 0; i < input.Length; i++)
+            {
+                var currentChar = input[i];
+                if (char.IsDigit(currentChar))
+                {
+                    token += currentChar;
+                    if (i ==input.Length -1 && leftSideInitialized)
+                    {
+                        expr.RightSideOperand = double.Parse(token);
+                        break;
+                    }
+
+                }else if (MathSymbol.Contains(currentChar))
+                {
+                    if (!leftSideInitialized)
+                    {
+
+
+                        expr.LeftSideOperand = double.Parse(token);
+                        leftSideInitialized = true;
+                    }
+                        expr.Operation = ParseMathOperation(currentChar.ToString());
+                        token = "";
+
+                }else if (currentChar == '-' && i>0)
+                {
+                   if (expr.Operation == MathOperation.None)
+                    {
+                        expr.Operation = MathOperation.Subtraction;
+                        expr.LeftSideOperand=double.Parse(token);
+                        leftSideInitialized = true;
+                        token = "";
+
+                    }else
+                    {
+                        token += currentChar;
+                    }
+
+                }
+                else if (char.IsLetter(currentChar))
+                {
+                    token += currentChar;
+                    leftSideInitialized  = true;
+
+                }else if (currentChar == ' ')
+                {
+                    if (!leftSideInitialized)
+                    {
+                        expr.LeftSideOperand= double.Parse(token);
+                        leftSideInitialized= true;
+                        token = "";
+                    }
+                    else if ( expr.Operation == MathOperation.None)
+                    {
+                        expr.Operation =ParseMathOperation(token);
+                        token = "";
+                    }
+                    
+                }else
+                {
+                    token += currentChar;
+                }
+            }
+            return expr;
+        }
+
+        private static MathOperation ParseMathOperation(string token)
+        {
+            switch (token.ToLower()) {
+                case "+":
+                    return MathOperation.Addition;
+                case "*":
+                    return MathOperation.Multiplication;
+                case "/":
+                    return MathOperation.Division;
+                case "%":
+                case "mod":
+                    return MathOperation.Modulus;
+                case "^":
+                case "pow":
+                    return MathOperation.Power;
+                case "sin":
+                    return MathOperation.Sin;
+                case "cos":
+                    return MathOperation.Cos;
+                case "tan":
+                    return MathOperation.Tan;
+                default:
+                    return MathOperation.None;
+            }
+        }
+    }
+}
---
+++ Simple Math Expression Evaluator/Simple Math Expression Evaluator/Program.cs
@@ -0,0 +1,48 @@
+﻿
+namespace Simple_Math_Expression_Evaluator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("Please enter the math experssion :");
+                var input = Console.ReadLine(); 
+                var expr = ExperssionParser.Parse(input);
+                Console.WriteLine($"left side :{expr.LeftSideOperand} , the operation :{expr.Operation} , right side :{expr.RightSideOperand}");
+                Console.WriteLine($"{input} = {EvaluateExperssion(expr)}");
+            }
+        }
+
+        private static object EvaluateExperssion(MathExpression expr)
+        {
+            if (expr.Operation == MathOperation.Addition)
+                return expr.LeftSideOperand + expr.RightSideOperand;
+            else if (expr.Operation == MathOperation.Subtraction)
+                return expr.LeftSideOperand - expr.RightSideOperand;
+            else if (expr.Operation == MathOperation.Division)
+                return expr.LeftSideOperand / expr.RightSideOperand;
+            else if ( expr.Operation == MathOperation.Modulus)
+                return expr.LeftSideOperand % expr.RightSideOperand;
+            else if (expr.Operation == MathOperation.Multiplication)
+                return expr.LeftSideOperand * expr.RightSideOperand;
+            else if (expr.Operation == MathOperation.Power)
+                return Math.Pow(expr.LeftSideOperand,expr.RightSideOperand);
+            else if (expr.Operation == MathOperation.Sin)
+                return Math.Sin(expr.RightSideOperand);
+            else if (expr.Operation == MathOperation.Cos)
+                return Math.Cos(expr.RightSideOperand);
+            else if (expr.Operation == MathOperation.Tan)
+                return Math.Tan(expr.RightSideOperand);
+
+
+            return 0;
+
+
+
+
+
+        }
+    }
+}
